bc2580505573f5b67418866bece1aae226566217
Rafael K <rknagao@gmail.com>
2022-04-06 19:15:03 -0300

    update gerais

---
+++ tera/classe/lib/mentoria_tera.py
@@ -0,0 +1,11 @@
+class numero:
+
+    def quadrado(x):
+        return x**2
+
+
+class print:
+
+    def bibliografia():
+        
+        return 'https://towardsdatascience.com/enhance-your-python-project-code-with-classes-5a19d0e9f841'
