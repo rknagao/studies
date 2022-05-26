7cada3f26697a13c39e117e353b08e0e8cb928cd
Rafael K <rknagao@gmail.com>
2022-05-26 07:35:58 -0300

    creating name & main example

---
+++ poop/main.py
@@ -0,0 +1,10 @@
+print('Example 1: name and main')
+print('executing main.py')
+
+if __name__ == '__main__':
+    print('i am main.')
+
+from not_main import not_main
+not_main()
+
+print('ending main.py')
